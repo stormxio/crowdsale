cef4d92a5a3c74f892ab798eb5c76522d974920e
Rui Maximo <rui@maximo.ws>
2017-10-05 19:39:45 -0700

    modified conversion rate, crowd sale number and company number

--- contracts/StormCrowdsale.sol
+++ contracts/StormCrowdsale.sol
@@ -7,15 +7,15 @@ contract StormCrowdsale is Crowdsale {
     presaleStartBlock; //TODO: set block
     crowdsaleEndedBlock; //TODO: set block
 
-    ethToTokenConversion = 27000;       // 1 ETH == 27,000 STORM tokens
+    ethToTokenConversion = 26950;       // 1 ETH == 26,950 STORM tokens
 
     maxTokenSupply = 10000000000;       // 10,000,000,000
     maxCommunityRoundCap = 945000000;   // without 15% bonus of 141,750,000
-    maxCrowdsaleCap = 2152810642; 
+    
+    maxCrowdsaleCap = 2137957682; 
     maxContribution = 350;              // maximum contribution during community round is ~$100,000 USD
 
-
-    uint256 companyTokens = 6760439358; // allocation to company, private presale and users (marketing)
+    uint256 companyTokens = 6775292318; // allocation to company, private presale and users (marketing)
     maxTokenSupply = maxTokenSupply.sub(companyTokens);     // reduce token supply after company allocation
     token.mintTokens(msg.sender, companyTokens);            // allocate tokens to company and users for marketing
   }
