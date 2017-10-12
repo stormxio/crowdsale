cbee656b928dc35d1d6d6438ec3bba5ce3ccb718
Rui Maximo <rui@maximo.ws>
2017-10-11 22:07:59 -0500

    Add files via upload

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,12 @@
+var Token = artifacts.require("./Token.sol");
+var StormToken = artifacts.require("./StormToken.sol");
 var Crowdsale = artifacts.require("./Crowdsale.sol");
 var StormCrowdsale = artifacts.require("./StormCrowdsale.sol");
 
 module.exports = function(deployer) {
+  deployer.deploy(Token);
+  deployer.link(Token, StormToken);
+  deployer.deploy(StormToken);
   deployer.deploy(Crowdsale);
   deployer.link(Crowdsale, StormCrowdsale);
   deployer.deploy(StormCrowdsale);
