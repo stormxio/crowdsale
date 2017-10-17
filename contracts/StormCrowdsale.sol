3d27711d11bc7c58c0d092eb741fd52642be450e
Rui Maximo <rui@maximo.ws>
2017-10-16 21:13:41 -0500

    final numbers
    final token numbers as reviewed with Arry and Simon.

--- contracts/StormCrowdsale.sol
+++ contracts/StormCrowdsale.sol
@@ -11,11 +11,11 @@ contract StormCrowdsale is Crowdsale {
     ethToTokenConversion = 26950;           // 1 ETH == 26,950 STORM tokens
 
     maxTokenSupply = 10000000000;           // 10,000,000,000
-    companyTokens = 6775292318;             // allocation to company, private presale and users (marketing)
+    companyTokens = 5799117100;             // allocation to company, private presale and users (marketing)
     maxTokenSupply -= companyTokens;        // reduce token supply after company allocation
 
     maxCommunityRoundCap = 945000000;       // without 15% bonus of 141,750,000
-    maxCrowdsaleCap = 3114132901;           // tokens allocated to crowdsale 
+    maxCrowdsaleCap = 3114132900;           // tokens allocated to crowdsale 
 
     maxEthCap = 206295;                     // maximum ETH to raise
     maxContribution = 100;                  // maximum contribution during community round
