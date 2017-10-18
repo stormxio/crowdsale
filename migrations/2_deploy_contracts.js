56b4e6da092149a7a519e18f52a5ac0272d3fec1
reoshibatsuji <reo.shibatsuji@gmail.com>
2017-10-18 12:40:48 -0500

    working tests

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -9,9 +9,9 @@ module.exports = function(deployer) {
   // });
   deployer.deploy(StormCrowdsale).then(function() {
     console.log('crowdsale address: ', StormCrowdsale.address);
-    // deployer.deploy(StormToken, StormCrowdsale.address).then(function() {
+    return deployer.deploy(StormToken, StormCrowdsale.address).then(function() {
     // deployer.deploy(StormToken, ).then(function() {
-    //   console.log('token address: ', StormToken.address);
-    // });
+      console.log('token address: ', StormToken.address);
+    });
   });
 };
