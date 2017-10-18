1ec3747c351bbce59d07adbb32bde77eb20d4ff1
reoshibatsuji <reo.shibatsuji@gmail.com>
2017-10-18 15:28:48 -0500

    Merge branch 'master' into makeTestWithJS
    # Conflicts:
    #	.gitignore
    #	contracts/Migrations.sol
    #	migrations/2_deploy_contracts.js
    #	test/primary_test.js
    #	test/test_send_payment.js
    #	truffle.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,8 @@
-
 var StormToken = artifacts.require("./StormToken.sol");
 var StormCrowdsale = artifacts.require("./StormCrowdsale.sol");
 
 module.exports = function(deployer) {
-  // deployer.deploy(StormCrowdsale).then(function() {
-  //   console.log('StormCrowdsale contract address: ', StormCrowdsale.address);
-  //   deployer.deploy(StormToken, StormCrowdsale.address);
-  // });
   deployer.deploy(StormCrowdsale).then(function() {
-    console.log('crowdsale address: ', StormCrowdsale.address);
-    return deployer.deploy(StormToken, StormCrowdsale.address).then(function() {
-    // deployer.deploy(StormToken, ).then(function() {
-      console.log('token address: ', StormToken.address);
-    });
+    return deployer.deploy(StormToken, StormCrowdsale.address);
   });
 };
--- test/primary_test.js
+++ test/primary_test.js
@@ -37,112 +37,51 @@ contract('Pending State', function(accounts) {
 		assert.equal(expected_state, curr_state, 'Checking if state is in Pending State');
 	});	
 
-	it('just to check before each', async function() {
-		console.log('just to check before each');
+	it('Check Contributor List non address', async function() {
+		let result = await crowdsale.contributorList(account3);
+		console.log('Contributor Data: ', result);
 	});
 
-	it('Sending transaction', async function() {
-		
+	it('Sending Transaction = User is refunded', async function() {
+		let initialEthBalanceOfUser = big2Number(await web3.eth.getBalance(account1));
+		let initialStormBalanceOfUser = big2Number(await stm.balanceOf(account1));
+		console.log('initial eth balance of user: ', initialEthBalanceOfUser);
+		console.log('initial storm balance of user: ', initialStormBalanceOfUser);
+
+		let initialTokenOfCrowdsale = big2Number(await crowdsale.tokenSold());
+		let initialEthOfCrowdsale = big2Number(await crowdsale.ethRaised());
+		console.log('initial token of crowdsale: ', initialTokenOfCrowdsale);
+		console.log('initial eth of crowdsale: ', initialEthOfCrowdsale);
+
+		let num_eth = 1; // 1 Eth
+
+		let receiptAddress = await web3.eth.sendTransaction({
+			from: account1,
+			to: StormCrowdsale.address,
+			value: num_eth,
+			gas: 4500000
+		});
+		console.log('Transaction should refund');
+		let transactionReceipt = await web3.eth.getTransactionReceipt(receiptAddress);
+		let gasUsed = transactionReceipt.gasUsed;
+		console.log('Transaction receipt: ', transactionReceipt);
+		console.log('Gas used: ', transactionReceipt.gasUsed);
+
+		let postEthBalanceOfUser = big2Number(await web3.eth.getBalance(account1));
+		let postStormBalanceOfUser = big2Number(await stm.balanceOf(account1));
+		console.log('post eth balance of user: ', postEthBalanceOfUser);
+		console.log('post storm balance of user: ', postStormBalanceOfUser);
+
+		let postTokenOfCrowdsale = big2Number(await crowdsale.tokenSold());
+		let postEthOfCrowdsale = big2Number(await crowdsale.ethRaised());
+		console.log('post token of crowdsale: ', postTokenOfCrowdsale);
+		console.log('post eth of crowdsale: ', postEthOfCrowdsale);
+
+		assert.equal(initialEthBalanceOfUser, postEthBalanceOfUser - gasUsed, 'check eth balance of user. should not change');
+		assert.equal(initialStormBalanceOfUser, postStormBalanceOfUser, 'check storm balance of user. should not change');
+		assert.equal(initialTokenOfCrowdsale, postTokenOfCrowdsale, 'check token balance of crowdsale. should not change');
+		assert.equal(initialEthOfCrowdsale, postEthOfCrowdsale, 'check eth raised of crowdsale. should not change');
 	});
-	// it('Setting Crowdsale Dates', async function() {
-	// 	// TODO: For all 3 States
-	// 	// Set the Community Round Start Date to before now
-	// 	//   => State is now Community Round
-	// 	crowdsale = await StormCrowdsale.deployed();
-	// 	let new_cRSD = 1608504400; // Change Epoch time to before now
-	// 	let new_cSD = 1608590800; // Change Epoch time to before now
-	// 	let new_cED = 1611182800; // Change Epoch time to after now
-	// 	await crowdsale.setCrowdsaleDates(new_cRSD, new_cSD, new_cED);
-		
-	// 	let currState = await crowdsale.getCrowdsaleState();
-	// 	// assert.equal()
-	// });
-
-	// it('Sending Payment to Crowdsale during Community Round', async function() {
-	// 	// Create a StormToken instance with the StormCrowdsale address
-	// 	// console.log('crowdsale address: ', crowdsale.address);
-	// 	let expected_address = await stm.crowdsaleContractAddress();
-	// 	console.log('expected address: ', expected_address);
-	// 	assert.equal(expected_address, crowdsale.address, 'check Crowdsale contract address');
-
-	// 	let currTimestamp = moment.utc().valueOf()/1000;
-	// 	await crowdsale.setCrowdsaleDates(currTimestamp - 100, currTimestamp + 9000000, currTimestamp + 900000001);
-	// 	let currState = await crowdsale.getCrowdsaleState();
-	// 	currState_number = new web3.BigNumber(currState).toString();
-	// 	let expectedState = 2; // Community Round = 2
-	// 	assert.equal(currState_number, expectedState, 'check state == community round');
-
-
-	// 	// Set Token on StormCrowdsale
-	// 	await crowdsale.setToken(StormToken.address);
-	// 	let tokenAddress = await crowdsale.getToken();
-	// 	assert.equal(StormToken.address, tokenAddress, 'check set token');
-	// 	// Add accounts into white list
-	// 	let accounts = [];
-	// 	accounts.push(account1);
-
-	// 	let bools = [];
-	// 	bools.push(true);
-	// 	await crowdsale.WhiteListContributors(accounts, bools);
-	// 	let contributorData = await crowdsale.contributorList(account1);
-	// 	// console.log('contributorData: ', contributorData);
-	// 	let expectedData = true;
-	// 	assert.equal(expectedData, contributorData[0], 'check contributor list and data');
-
-	// 	// Send Payment transaction
-	// 	// Check before and after balance of the account, tokenSold, ethRaised
-	// 	let initial_balance = await web3.eth.getBalance(account1);
-	// 	let initial_balance_number = new web3.BigNumber(initial_balance).toString();
-	// 	let initial_token = await crowdsale.tokenSold();
-	// 	let initial_token_number = new web3.BigNumber(initial_token).toString();
-	// 	let initial_eth = await crowdsale.ethRaised();
-	// 	let initial_eth_number = new web3.BigNumber(initial_eth).toString();
-	// 	console.log('initial balance: ', initial_balance);
-	// 	console.log('initial token: ', initial_token);
-	// 	console.log('initial eth in crowdsale: ', initial_eth);
-	// 	console.log('initial balance number: ', initial_balance_number);
-	// 	console.log('initial token number: ', initial_token_number);
-	// 	console.log('initial eth number in crowdsale: ', initial_eth_number);
-
-	// 	// let value = web3.toWei(1, 'wei');
-	// 	// let value_number = new web3.BigNumber(value).toString();
-	// 	// console.log('value: ', value);
-	// 	// console.log('value number: ', value_number);
-	// 	let value = 100; // 1 eth
-		
-
-	// 	console.log('address: ', StormCrowdsale.address);
-	// 	let initial_storm = await stm.balanceOf(account1);
-	// 	let initial_storm_number = new web3.BigNumber(initial_storm).toString();
-	// 	console.log('token balance: ', initial_storm);
-	// 	console.log('token balance number: ', initial_storm_number);
-
-	// 	web3.eth.sendTransaction({
-	// 		from: account1,
-	// 		to: StormCrowdsale.address,
-	// 		value: value,
-	// 		gas: 4500000 //4,500,000
-	// 	});
-
-	// 	let post_balance = await web3.eth.getBalance(account1);
-	// 	let post_balance_number = new web3.BigNumber(post_balance).toString();
-
-	// 	let post_token = await crowdsale.tokenSold();
-	// 	let post_token_number = new web3.BigNumber(post_token).toString();
-
-	// 	let post_ethRaised = await crowdsale.ethRaised();
-	// 	let post_ethRaised_number = new web3.BigNumber(post_ethRaised).toString();
-
-	// 	console.log('test post_balance: ', big2Number(await web3.eth.getBalance(account1)));
-
-	// 	console.log('post balance: ', post_balance);
-	// 	console.log('post token: ', post_token);
-	// 	console.log('post eth in crowdsale: ', post_ethRaised);
-
-	// 	console.log('post balance number: ', post_balance_number);
-	// 	console.log('post token number: ', post_token_number);
-	// 	console.log('post eth number in crowdsale: ', post_ethRaised_number);
-	// });
 });
 
 function big2Number(bigNumber) {
