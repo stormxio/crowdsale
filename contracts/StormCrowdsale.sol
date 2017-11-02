40099e705b4b9f5ab79b8506ffe37d02c467275e
Rui Maximo <rui@maximo.ws>
2017-11-02 14:04:35 -0500

    update crowdsale numbers per Arry/Simon

--- contracts/StormCrowdsale.sol
+++ contracts/StormCrowdsale.sol
@@ -13,11 +13,12 @@ contract StormCrowdsale is Crowdsale {
     ethToTokenConversion = 26950;                 // 1 ETH == 26,950 STORM tokens
 
     maxTokenSupply = 10000000000 ether;           // 10,000,000,000
-    companyTokens = 5799117100 ether;             // allocation to company, private presale and users (marketing)
+    companyTokens = 8124766171 ether;             // allocation for company pool, private presale, user pool 
+                                                  // 2,325,649,071 tokens from the company pool are voluntarily locked for 2 years
 
     maxCommunityWithoutBonusCap = 945000000 ether;
     maxCommunityCap = 1086750000 ether;           // 945,000,000 with 15% bonus of 141,750,000
-    maxCrowdsaleCap = 3114132900 ether;           // tokens allocated to crowdsale 
+    maxCrowdsaleCap = 788483829 ether;            // tokens allocated to crowdsale 
 
     maxContribution = 100 ether;                  // maximum contribution during community round
   }
